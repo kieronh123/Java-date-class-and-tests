c372adfea5730b97a39bfbe0dfe3859fd044dff1
Kieron Hushon <sc16kh@leeds.ac.uk>
2017-10-25 01:00:23 +0100

    Cleaned up code / made code more consistent in it's style

--- src/comp2931/cwk1/Date.java
+++ src/comp2931/cwk1/Date.java
@@ -12,89 +12,99 @@ public class Date {
     private static final int DAYS_PER_MONTH = 31;
     private static final int MONTHS_PER_YEAR = 12;
 
+    private int year;
+    private int month;
+    private int day;
 
-  private int year;
-  private int month;
-  private int day;
+    /**
+    * Creates a date using the given values for year, month and day.
+    *
+    * @param y Year
+    * @param m Month
+    * @param d Day
+    */
+    public Date(int y, int m, int d) throws IllegalArgumentException
+    {
+        if (!validDate(y, m, d))
+        {
+            throw new IllegalArgumentException("Invalid Date given");
+        }
 
-  /**
-   * Creates a date using the given values for year, month and day.
-   *
-   * @param y Year
-   * @param m Month
-   * @param d Day
-   */
-  public Date(int y, int m, int d) throws IllegalArgumentException
-  {
+        year = y;
+        month = m;
+        day = d;
+    }
 
+    /**
+    * Returns the year component of this date.
+    *
+    * @return Year
+    */
+    public int getYear()
+    {
+        return year;
+    }
 
-        if (!validDate(y, m, d)) throw new IllegalArgumentException("Invalid Date given");
+    /**
+    * Returns the month component of this date.
+    *
+    * @return Month
+    */
+    public int getMonth()
+    {
+        return month;
+    }
 
-          year = y;
-          month = m;
-          day = d;
+    /**
+    * Returns the day component of this date.
+    *
+    * @return Day
+    */
+    public int getDay()
+    {
+        return day;
+    }
 
+    /**
+    * Provides a string representation of this date.
+    *
+    * ISO 8601 format is used (YYYY-MM-DD).
+    *
+    * @return Date as a string
+    */
+    @Override
+    public String toString()
+    {
+        return String.format("%04d-%02d-%2d", year, month, day);
+    }
 
-  }
+    /**
+    * Returns true or false depending on if the date is
+    * valid or not
+    *
+    * @param y Year
+    * @param m Month
+    * @param d Date
+    *
+    * @return true if the date is valid, false if the date is not valid
+    */
+    public boolean validDate(int y, int m, int d)
+    {
+        if (y < 0)
+        {
+            return false;
+        }
 
-  /**
-   * Returns the year component of this date.
-   *
-   * @return Year
-   */
-  public int getYear()
-  {
-    return year;
-  }
+        if ((m < 1) || (m > 12))
+        {
+            return false;
+        }
 
-  /**
-   * Returns the month component of this date.
-   *
-   * @return Month
-   */
-  public int getMonth()
-  {
-    return month;
-  }
+        if ((d < 1) || (d > 31))
+        {
+            return false;
+        }
 
-  /**
-   * Returns the day component of this date.
-   *
-   * @return Day
-   */
-  public int getDay()
-  {
-    return day;
-  }
-
-  /**
-   * Provides a string representation of this date.
-   *
-   * ISO 8601 format is used (YYYY-MM-DD).
-   *
-   * @return Date as a string
-   */
-  @Override
-  public String toString()
-  {
-    return String.format("%04d-%02d-%2d", year, month, day);
-  }
-
-  /**
-   * Returns true or false depending on if the date is
-   * valid or not
-   *
-   * @param y Year
-   * @param m Month
-   * @param d Date
-   *
-   * @return true if the date is valid, false if the date is not valid
-   */
-  public boolean validDate(int y, int m, int d)
-  {
-        if (y < 0) return false;
-        if ((m < 1) || (m > 12)) return false;
-        if ((d < 1) || (d > 31)) return false;
         switch (m)
         {
             case 1: return true;
@@ -123,16 +133,20 @@ public class Date {
      * @return True if this Date object is equal to the other, false otherwise
      */
     @Override
-    public boolean equals(Object other) {
-      if (other == this) {
+    public boolean equals(Object other)
+    {
+      if (other == this)
+      {
         // 'other' is same object as this one!
         return true;
       }
-      else if (! (other instanceof Date)) {
+      else if (! (other instanceof Date))
+      {
         // 'other' is not a Time object
         return false;
       }
-      else {
+      else
+      {
         // Compare fields
         Date otherDate = (Date) other;
         return getYear() == otherDate.getYear()
--- src/comp2931/cwk1/DateTest.java
+++ src/comp2931/cwk1/DateTest.java
@@ -11,61 +11,63 @@ import static org.junit.Assert.*;
  * Unit tests for the Date class.
  */
 
-public class DateTest {
+public class DateTest
+{
 
     private Date firstDay;
     private Date lastDay;
 
 
   @Before
-  public void setUp() {
-    firstDay = new Date(2017,1,1);
-    lastDay = new Date(2017,12,31);
+  public void setUp()
+  {
+      firstDay = new Date(2017,1,1);
+      lastDay = new Date(2017,12,31);
   }
-
-
-
 
 
   @Test
-  public void timeToString() {
-    assertThat(firstDay.toString(), is("2017-01- 1"));
-    assertThat(lastDay.toString(), is("2017-12-31"));
+  public void timeToString()
+  {
+      assertThat(firstDay.toString(), is("2017-01- 1"));
+      assertThat(lastDay.toString(), is("2017-12-31"));
   }
 
   @Test(expected=IllegalArgumentException.class)
-  public void yearTooLow() {
-    new Date(-1,1,1);
+  public void yearTooLow()
+  {
+      new Date(-1,1,1);
   }
 
   @Test(expected=IllegalArgumentException.class)
-  public void monthTooLow() {
-    new Date(2017,-1,10);
+  public void monthTooLow()
+  {
+      new Date(2017,-1,10);
   }
 
   @Test(expected=IllegalArgumentException.class)
-  public void monthTooHigh() {
-    new Date(2017,13,15);
+  public void monthTooHigh()
+  {
+      new Date(2017,13,15);
   }
 
   @Test(expected=IllegalArgumentException.class)
-  public void dayTooLow() {
-    new Date(2017,3,-1);
+  public void dayTooLow()
+  {
+      new Date(2017,3,-1);
   }
 
   @Test(expected=IllegalArgumentException.class)
-  public void dayTooHigh() {
-    new Date(2017,5,32);
+  public void dayTooHigh()
+  {
+      new Date(2017,5,32);
   }
 
   @Test(expected=IllegalArgumentException.class)
-  public void invalidDate() {
-    new Date(2017,4,31);
+  public void invalidDate()
+  {
+      new Date(2017,4,31);
   }
-
-  
-
-
 
 
 }
